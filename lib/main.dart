e0a87e4e179c3d3425900b658e1e090d34e42edc
krish lanjewar <lanjewarkrish07@gmail.com>
2025-12-30 10:02:24 +0530

    home page dashbord ui is bing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'src/core/theme/app_theme.dart';
 import 'src/features/auth/presentation/login_page.dart';
+import 'src/features/dashboard/home/pharmacy_home_screen.dart';
 
 void main() {
   runApp(const MedShakthiApp());
@@ -16,7 +17,7 @@ class MedShakthiApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const LoginPage(),
+      home: const PharmacyHomeScreen(),
     );
   }
 }
